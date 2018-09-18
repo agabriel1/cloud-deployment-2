6a12c4c7bd0af50980ce8ca132aa38ee484d4d78
agabriel1 <43051089+agabriel1@users.noreply.github.com>
2018-09-18 13:17:39 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -7,8 +7,8 @@ request = portal.context.makeRequestRSpec()
 
 node1 = request.XenVM("node1")
 iface1 = node1.addInterface("if1")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-node.routable_control_ip = "true"
+node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+node1.routable_control_ip = "true"
 
 # Specify the component id and the IPv4 address
 iface1.component_id = "eth1"
@@ -16,7 +16,7 @@ iface1.addAddress(rspec.IPv4Address("192.168.1.1", "255.255.255.0"))
 
 node2 = request.XenVM("node2")
 iface2 = node2.addInterface("if2")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 
 # Specify the component id and the IPv4 address
 iface2.component_id = "eth2"
@@ -24,7 +24,7 @@ iface2.addAddress(rspec.IPv4Address("192.168.1.2", "255.255.255.0"))
 
 node3 = request.XenVM("node3")
 iface3 = node3.addInterface("if3")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+node3.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 
 # Specify the component id and the IPv4 address
 iface3.component_id = "eth3"
@@ -32,7 +32,7 @@ iface3.addAddress(rspec.IPv4Address("192.168.1.3", "255.255.255.0"))
 
 node4 = request.XenVM("node-4")
 iface4 = node4.addInterface("if4")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+node4.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 
 # Specify the component id and the IPv4 address
 iface4.component_id = "eth4"
